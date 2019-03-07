0f30a46feae26cbd401506c62c0a0435fe9cfeb7
Achintsharan Bhagat <P17157525@my365.dmu.ac.uk>
2019-03-07 21:35:19 +0000

    Added Boxes

--- BusinessIntelligenceLabs/BusinessIntelligenceLabs/Form1.Designer.cs
+++ BusinessIntelligenceLabs/BusinessIntelligenceLabs/Form1.Designer.cs
@@ -28,25 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblhello = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // lblhello
+            // textBox1
             // 
-            this.lblhello.AutoSize = true;
-            this.lblhello.Location = new System.Drawing.Point(403, 247);
-            this.lblhello.Name = "lblhello";
-            this.lblhello.Size = new System.Drawing.Size(34, 13);
-            this.lblhello.TabIndex = 0;
-            this.lblhello.Text = "Hello ";
-            this.lblhello.Click += new System.EventHandler(this.label1_Click);
+            this.textBox1.Location = new System.Drawing.Point(361, 138);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(361, 165);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(361, 191);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.lblhello);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -56,7 +71,9 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lblhello;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
     }
 }
 
