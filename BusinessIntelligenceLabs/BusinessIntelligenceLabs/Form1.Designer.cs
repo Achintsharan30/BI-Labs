dff11a0f841f536a9df5305f0f402b4d3c3cb324
Achintsharan Bhagat <P17157525@my365.dmu.ac.uk>
2019-03-07 23:43:48 +0000

    dataset

--- BusinessIntelligenceLabs/BusinessIntelligenceLabs/Form1.Designer.cs
+++ BusinessIntelligenceLabs/BusinessIntelligenceLabs/Form1.Designer.cs
@@ -31,6 +31,8 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.buttonGetDate = new System.Windows.Forms.Button();
+            this.listBoxDates = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // textBox1
@@ -54,11 +56,32 @@
             this.textBox3.Size = new System.Drawing.Size(100, 20);
             this.textBox3.TabIndex = 2;
             // 
+            // buttonGetDate
+            // 
+            this.buttonGetDate.Location = new System.Drawing.Point(361, 241);
+            this.buttonGetDate.Name = "buttonGetDate";
+            this.buttonGetDate.Size = new System.Drawing.Size(75, 23);
+            this.buttonGetDate.TabIndex = 3;
+            this.buttonGetDate.Text = "Gets Dates";
+            this.buttonGetDate.UseVisualStyleBackColor = true;
+            this.buttonGetDate.Click += new System.EventHandler(this.buttonGetDate_Click);
+            // 
+            // listBoxDates
+            // 
+            this.listBoxDates.FormattingEnabled = true;
+            this.listBoxDates.Location = new System.Drawing.Point(361, 291);
+            this.listBoxDates.Name = "listBoxDates";
+            this.listBoxDates.Size = new System.Drawing.Size(120, 95);
+            this.listBoxDates.TabIndex = 4;
+          
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBoxDates);
+            this.Controls.Add(this.buttonGetDate);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -74,6 +97,8 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button buttonGetDate;
+        private System.Windows.Forms.ListBox listBoxDates;
     }
 }
 
--- BusinessIntelligenceLabs/BusinessIntelligenceLabs/Form1.cs
+++ BusinessIntelligenceLabs/BusinessIntelligenceLabs/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -21,5 +22,40 @@ namespace BusinessIntelligenceLabs
         {
 
         }
+
+        private void buttonGetDate_Click(object sender, EventArgs e)
+        {
+            //create a list to store my  data in 
+            List<string> Dates = new List<string>();
+            //clear the list box to ensure no old data is in there.
+            listBoxDates.Items.Clear();
+
+            //create a database string
+            string connectionString = Properties.Settings.Default.Data_set_1_1_ConnectionString;
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                connection.Open();
+                OleDbDataReader reader = null;
+                OleDbCommand getDates = new OleDbCommand("SELECT [Order Date], [Ship Date], [Ship Date] from Sheet1", connection);
+
+                reader = getDates.ExecuteReader();
+                while (reader.Read())
+                {
+                    Dates.Add(reader[0].ToString());
+                    Dates.Add(reader[1].ToString());
+                }
+            }
+            List<string> DatesFormatted = new List<string>();
+            foreach(string date in Dates)
+            {
+                var dates =  date.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+                DatesFormatted.Add(dates[0]);
+            }
+            listBoxDates.DataSource = DatesFormatted;
+        }
+
+        
+        
+
+        }
     }
-}
