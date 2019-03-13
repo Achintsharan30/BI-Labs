e11ca15da77a827d3dcc919b3a32722c4d9c47fb
Achintsharan Bhagat <P17157525@my365.dmu.ac.uk>
2019-03-13 22:54:05 +0000

    information out from database and using it

--- BusinessIntelligenceLabs/BusinessIntelligenceLabs/Form1.Designer.cs
+++ BusinessIntelligenceLabs/BusinessIntelligenceLabs/Form1.Designer.cs
@@ -33,32 +33,37 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.buttonGetDate = new System.Windows.Forms.Button();
             this.listBoxDates = new System.Windows.Forms.ListBox();
+            this.btnGetDateSource = new System.Windows.Forms.Button();
+            this.listBoxDate = new System.Windows.Forms.ListBox();
+            this.btnGetDateDimension = new System.Windows.Forms.Button();
+            this.listBoxDatesDimension = new System.Windows.Forms.ListBox();
+            this.btnBuildFactTable = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(361, 138);
+            this.textBox1.Location = new System.Drawing.Point(644, 81);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 0;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(361, 165);
+            this.textBox2.Location = new System.Drawing.Point(644, 117);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 1;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(361, 191);
+            this.textBox3.Location = new System.Drawing.Point(644, 165);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 20);
             this.textBox3.TabIndex = 2;
             // 
             // buttonGetDate
             // 
-            this.buttonGetDate.Location = new System.Drawing.Point(361, 241);
+            this.buttonGetDate.Location = new System.Drawing.Point(644, 205);
             this.buttonGetDate.Name = "buttonGetDate";
             this.buttonGetDate.Size = new System.Drawing.Size(75, 23);
             this.buttonGetDate.TabIndex = 3;
@@ -69,17 +74,68 @@
             // listBoxDates
             // 
             this.listBoxDates.FormattingEnabled = true;
-            this.listBoxDates.Location = new System.Drawing.Point(361, 291);
+            this.listBoxDates.Location = new System.Drawing.Point(644, 271);
             this.listBoxDates.Name = "listBoxDates";
             this.listBoxDates.Size = new System.Drawing.Size(120, 95);
             this.listBoxDates.TabIndex = 4;
-          
+            // 
+            // btnGetDateSource
+            // 
+            this.btnGetDateSource.Location = new System.Drawing.Point(36, 165);
+            this.btnGetDateSource.Name = "btnGetDateSource";
+            this.btnGetDateSource.Size = new System.Drawing.Size(138, 39);
+            this.btnGetDateSource.TabIndex = 5;
+            this.btnGetDateSource.Text = "Get Dates from Source";
+            this.btnGetDateSource.UseVisualStyleBackColor = true;
+            // 
+            // listBoxDate
+            // 
+            this.listBoxDate.FormattingEnabled = true;
+            this.listBoxDate.Location = new System.Drawing.Point(36, 222);
+            this.listBoxDate.Name = "listBoxDate";
+            this.listBoxDate.Size = new System.Drawing.Size(138, 95);
+            this.listBoxDate.TabIndex = 6;
+            // 
+            // btnGetDateDimension
+            // 
+            this.btnGetDateDimension.Location = new System.Drawing.Point(213, 163);
+            this.btnGetDateDimension.Name = "btnGetDateDimension";
+            this.btnGetDateDimension.Size = new System.Drawing.Size(149, 41);
+            this.btnGetDateDimension.TabIndex = 8;
+            this.btnGetDateDimension.Text = "Get Dates from Dimension";
+            this.btnGetDateDimension.UseVisualStyleBackColor = true;
+            this.btnGetDateDimension.Click += new System.EventHandler(this.btnGetDateDimension_Click);
+            // 
+            // listBoxDatesDimension
+            // 
+            this.listBoxDatesDimension.FormattingEnabled = true;
+            this.listBoxDatesDimension.HorizontalScrollbar = true;
+            this.listBoxDatesDimension.Location = new System.Drawing.Point(213, 222);
+            this.listBoxDatesDimension.Name = "listBoxDatesDimension";
+            this.listBoxDatesDimension.Size = new System.Drawing.Size(149, 95);
+            this.listBoxDatesDimension.TabIndex = 9;
+            this.listBoxDatesDimension.SelectedIndexChanged += new System.EventHandler(this.listBoxDatesDimension_SelectedIndexChanged_1);
+            // 
+            // btnBuildFactTable
+            // 
+            this.btnBuildFactTable.Location = new System.Drawing.Point(392, 165);
+            this.btnBuildFactTable.Name = "btnBuildFactTable";
+            this.btnBuildFactTable.Size = new System.Drawing.Size(106, 39);
+            this.btnBuildFactTable.TabIndex = 10;
+            this.btnBuildFactTable.Text = "Build Fact Table";
+            this.btnBuildFactTable.UseVisualStyleBackColor = true;
+            this.btnBuildFactTable.Click += new System.EventHandler(this.btnBuildFactTable_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBuildFactTable);
+            this.Controls.Add(this.listBoxDatesDimension);
+            this.Controls.Add(this.btnGetDateDimension);
+            this.Controls.Add(this.listBoxDate);
+            this.Controls.Add(this.btnGetDateSource);
             this.Controls.Add(this.listBoxDates);
             this.Controls.Add(this.buttonGetDate);
             this.Controls.Add(this.textBox3);
@@ -99,6 +155,11 @@
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button buttonGetDate;
         private System.Windows.Forms.ListBox listBoxDates;
+        private System.Windows.Forms.Button btnGetDateSource;
+        private System.Windows.Forms.ListBox listBoxDate;
+        private System.Windows.Forms.Button btnGetDateDimension;
+        private System.Windows.Forms.ListBox listBoxDatesDimension;
+        private System.Windows.Forms.Button btnBuildFactTable;
     }
 }
 
--- BusinessIntelligenceLabs/BusinessIntelligenceLabs/Form1.cs
+++ BusinessIntelligenceLabs/BusinessIntelligenceLabs/Form1.cs
@@ -14,6 +14,8 @@ namespace BusinessIntelligenceLabs
 {
     public partial class Form1 : Form
     {
+        private object dayNumber;
+
         public Form1()
         {
             InitializeComponent();
@@ -29,6 +31,7 @@ namespace BusinessIntelligenceLabs
 
             DateTime myDate = new DateTime(Year, Month, Day);
             string dDate = myDate.ToString("M/dd/yyyy");
+
             //create a connection to MDF file
             String connectionStringDestination = Properties.Settings.Default.DestinationDatabase_1_ConnectionString;
             using (SqlConnection myConnection = new SqlConnection(connectionStringDestination))
@@ -54,10 +57,10 @@ namespace BusinessIntelligenceLabs
                         return Convert.ToInt32(reader["id"]);
                     }
                 }
-               
-          
+
+
             }
-                    return 0;
+            return 0;
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -84,10 +87,16 @@ namespace BusinessIntelligenceLabs
             Boolean weekend = false;
             if (dayOfweek == "Saturday" || dayOfweek == "Sunday") weekend = true;
 
+            // convert this database friendly format 
+            string dbDate = myDate.ToString("M/dd/yyyy");
+            insertTimeDimension(dbDate, dayOfweek, dayNumber, monthName, Month, Day, weekNumber, Year, weekend, dayOfYear);
+
         }
-  
-        // convert this database friendly format 
-        string dbDate = myDate.ToString("M/dd/yyyy");
+
+        private void insertTimeDimension(string dbDate, string dayOfweek, object dayNumber, string monthName, int month, int day, int weekNumber, int year, bool weekend, int dayOfYear)
+        {
+            throw new NotImplementedException();
+        }
 
         private void insertTimeDimension(string date, string dayName, Int32 dayNumber, string monthName, Int32 monthNumber, Int32 weekNumber, Int32 year, Boolean weekend, Int32 dayOfYear)
         {
@@ -101,17 +110,17 @@ namespace BusinessIntelligenceLabs
                 //check if the data is already exists in the database - we dont need to duplicates 
                 SqlCommand command = new SqlCommand("SELECT id FROM Time Where date = @date", myConnection);
                 command.Parameters.Add(new SqlParameter("date", date));
-          
+
 
                 //create a variable and assign it false 
-                Boolean exists = false; 
+                Boolean exists = false;
 
                 // run the command and read the results 
                 using (SqlDataReader reader = command.ExecuteReader())
                 {
                     //if there are rows it means the data exists, so updtaes the var!
                     if (reader.HasRows) exists = true;
-                   
+
                 }
                 if (exists == false)
                 {
@@ -120,7 +129,7 @@ namespace BusinessIntelligenceLabs
                     insertCommand.Parameters.Add(new SqlParameter("dayName", dayName));
                     insertCommand.Parameters.Add(new SqlParameter("dayNumber", dayNumber));
                     insertCommand.Parameters.Add(new SqlParameter("monthName", monthNumber));
-                    insertCommand.Parameters.Add(new SqlParameter("monthNumber",monthNumber));
+                    insertCommand.Parameters.Add(new SqlParameter("monthNumber", monthNumber));
                     insertCommand.Parameters.Add(new SqlParameter("weekNumber", weekNumber));
                     insertCommand.Parameters.Add(new SqlParameter("year", year));
                     insertCommand.Parameters.Add(new SqlParameter("weekend", weekend));
@@ -130,7 +139,7 @@ namespace BusinessIntelligenceLabs
                     int recordsAffected = insertCommand.ExecuteNonQuery();
                     Console.WriteLine("Records affected: + records affected");
                 }
-        
+
 
             }
         }
@@ -161,9 +170,9 @@ namespace BusinessIntelligenceLabs
                 }
             }
             List<string> DatesFormatted = new List<string>();
-            foreach(string date in Dates)
+            foreach (string date in Dates)
             {
-                var dates =  date.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+                var dates = date.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
                 DatesFormatted.Add(dates[0]);
             }
 
@@ -171,18 +180,118 @@ namespace BusinessIntelligenceLabs
             listBoxDates.DataSource = DatesFormatted;
 
             //split the date and insert every dates in the list 
-            foreach(string date in DatesFormatted)
+            foreach (string date in DatesFormatted)
             {
                 splitDates(date);
             }
             splitDates(DatesFormatted[0]);
 
-         
+
 
 
             string fullDate = DatesFormatted[0].ToString();
             string[] arrayDate1 = fullDate.Split('/');
             Console.WriteLine("Day:" + arrayDate1[0] + "Month:" + arrayDate1[1] + "Year:" + arrayDate1[2]);
         }
-}
+
+        private void Listboxdatesdimension_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGetDateDimension_Click(object sender, EventArgs e)
+        {
+            //create a list to data store in
+            List<string> DestinationDates = new List<string>();
+            //clear the listbox to ensure no old data is in there.
+            listBoxDatesDimension.DataSource = null;
+            listBoxDatesDimension.Items.Clear();
+
+            //create a connection to MDF file
+            String connectionStringDestination = Properties.Settings.Default.DestinationDatabase_1_ConnectionString;
+            using (SqlConnection myConnection = new SqlConnection(connectionStringDestination))
+            {
+                //open SQL connection 
+                myConnection.Open();
+
+                //check if the data is already exists in the database - we dont need to duplicates 
+
+                SqlCommand command = new SqlCommand("SELECT id, dayName, dayNumber, monthName, monthNumber, weekNumber, year," + "weekend, date, dayOfYear FROM Time", myConnection);
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read()) ;// this loop actually gets the data
+                        //do something with each record here
+                        //build what i want the listbox to show 
+                        string id = reader["id"].ToString();
+                        string dayName = reader["dayName"].ToString();
+                        string dayNumber = reader["dayNumber"].ToString();
+                        string monthName = reader["monthName"].ToString();
+                        string monthNumber = reader["monthNumber"].ToString();
+                        string weekNumber = reader["weekNumber"].ToString();
+                        string year = reader["year"].ToString();
+                        string weekend = reader["weekend"].ToString();
+                        string date = reader["date"].ToString();
+                        string dayOfYear = reader["dayOfYear"].ToString();
+
+                        string text;
+
+                        text = "ID = " + id + ", Day Name =" + dayName + ", DayNumber = " + dayNumber +
+                            ", Month Name = " + monthName + ", Month Number= " + monthNumber + ", week Number= " + weekNumber +
+                            ", Year= " + year + ", Weekend= " + weekend + ", Date = " + date + "Day of year =" + dayOfYear;
+
+                        DestinationDates.Add(text);
+                    }
+
+                    else // there was no data - show an error may me ??
+                    {
+                        DestinationDates.Add("No data present in the Time Dimension");
+
+                    }
+                }
+
+            }
+
+            //bind the listbox to the list 
+            listBoxDatesDimension.DataSource = DestinationDates;
+        }
+
+        private void listBoxDatesDimension_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void InsertFactTablen(Int32 ProductId, Int32 TimeId, Int32 CustomerId, Decimal Sales, Int32 Quantity, Decimal Profit, Decimal Discount)
+        {
+            //create the database string
+            string connectionString = Properties.Settings.Default.Data_set_1_1_ConnectionString;
+
+            using (OleDbConnection connection = new OleDbConnection(connectionString))
+            {
+                connection.Open();
+                OleDbDataReader reader = null;
+                OleDbCommand getDates = new OleDbCommand("SELECT ID, [Row ID], [Order ID], [Order Date], [Ship Date]," +
+                    "[Ship Mode], [Customer ID] , [Customer Name], Segment, Country, City, State, [Postal Code], [Product ID]," +
+                    "Region, Category, [Sub-Category], [Product Name], Sales, Quantity, Profit, Discount FROM Sheet1", connection);
+
+                reader = getDates.ExecuteReader();
+                while (reader.Read())
+                {
+                    // get a line of data fro, the source
+                    // get the numeric values
+                    Decimal sales = Convert.ToDecimal(reader["Sales"]);
+                    Int32 quantity = Convert.ToInt32(reader["Quantity"]);
+                    Decimal profit = Convert.ToDecimal(reader["profit"]);
+                    Decimal discount = Convert.ToDecimal(reader["Discount"]);
+                    //get the dimension Ids
+                    Int32 productId = 0;
+                    Int32 timeId = GetDatesId(reader["Order Date"].ToString());
+                    Int32 customerId = 0;
+                    // Insert it into the database 
+                }
+            }
+        }
     }
+}
+
